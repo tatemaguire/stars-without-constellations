0c8a82b9309338231f2e750230ba78519831fadc
Tate Maguire <tatemaguir@gmail.com>
2026-03-06 00:53:01 -0800

    fixed tileset bug. cleaned terrain_colorer.gd. got rid of ridiculous coding plugin :(

--- scripts/terrain_colorer.gd
+++ scripts/terrain_colorer.gd
@@ -1,20 +1,15 @@
 extends TileMapLayer
-## THIS IS MY SCRIPT
-
-class_name TerrainColorer
 
 enum TerrainColor {GREEN, VIOLET, TEAL, PURPLE}
 
-var current_terrain_color = TerrainColor.GREEN
-
+@export var current_terrain_color: TerrainColor = TerrainColor.GREEN
 
 func set_terrain_color(color: TerrainColor):
-	
-	print(color, " ", TerrainColor.size())
 	current_terrain_color = color
-	return null
-
 
 func _input(event: InputEvent):
 	if event.is_action_pressed("Jump"):
-		set_terrain_color(current_terrain_color+1)
+		var new_col = current_terrain_color + 1
+		new_col %= TerrainColor.size()
+		print(new_col)
+		set_terrain_color(new_col)
