aa3564e469a7873619721ff4a9f5b875888e3a09
Tate Maguire <tatemaguir@gmail.com>
2026-03-08 11:36:20 -0700

    camera fixes. gravity changed

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,17 +2,19 @@ extends Camera2D
 
 @export var _player_character: PlayerCharacter
 
+@onready var _room_size: Vector2i = ProjectSettings.get_setting("global/room_size")
+
 func _process(_delta) -> void:
 	var p_screen_pos: Vector2i = Vector2i(_player_character.position - position)
 	
 	# Check if camera needs to move in the x-direction
 	if p_screen_pos.x < 0:
-		position.x -= Global.room_size.x
-	elif p_screen_pos.x > Global.room_size.x:
-		position.x += Global.room_size.x
+		position.x -= _room_size.x
+	elif p_screen_pos.x > _room_size.x:
+		position.x += _room_size.x
 	
 	# Check if camera needs to move in the y-direction
 	if p_screen_pos.y < 0:
-		position.y -= Global.room_size.y
-	elif p_screen_pos.y > Global.room_size.y:
-		position.y += Global.room_size.y
+		position.y -= _room_size.y
+	elif p_screen_pos.y > _room_size.y:
+		position.y += _room_size.y
