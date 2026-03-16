578f5e3374ac498df3f3c7b4c022fe6f0424edab
Tate Maguire <tatemaguir@gmail.com>
2026-03-16 10:24:47 -0700

    enemy physics fixes

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var speed: float = 40
 @export var damage: int = 1
@@ -6,31 +6,37 @@ extends RigidBody2D
 
 var direction: int = 1
 
-func _ready() -> void:
-	$AnimatedSprite2D.play("run")
-	contact_monitor = true
-	max_contacts_reported = 4
+func _physics_process(delta: float) -> void:
+	_set_velocity(delta)
+	move_and_slide()
+	_process_collisions()
 
 
-func _physics_process(_delta: float) -> void:
-	if direction == 0:
-		printerr("ENEMY STAGNANT")
-	# Change direction when run into wall
-	if $LeftCast.is_colliding():
-		direction = 1
-		$AnimatedSprite2D.flip_h = false
-	if $RightCast.is_colliding():
-		direction = -1
-		$AnimatedSprite2D.flip_h = true
+func _set_velocity(delta: float) -> void:
+	velocity.x = direction * speed
+	velocity += get_gravity() * delta
+
+
+func _process_collisions() -> void:
+	# Turn around if on a wall
+	if is_on_wall():
+		var wall_normal = get_wall_normal()
+		if wall_normal.x < 0: # Wall is to the right
+			direction = -1
+			$AnimatedSprite2D.flip_h = true
+		else: # Wall is to the left
+			direction = 1
+			$AnimatedSprite2D.flip_h = false
 	
-	# Check collisions
-	for body in get_colliding_bodies():
-		if body is PlayerCharacter:
+	# Process collision with player
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider is PlayerCharacter:
 			# calculate knockback velocity
-			var knockback: Vector2 = (body.global_position - self.global_position).normalized()
+			var knockback: Vector2 = (collider.global_position - self.global_position).normalized()
 			knockback = knockback.normalized() * knockback_velocity
 			# apply damage and knockback
-			body.take_damage(damage, knockback)
-
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	state.linear_velocity.x = direction * speed
+			collider.take_damage(damage, knockback)
+		
+		
